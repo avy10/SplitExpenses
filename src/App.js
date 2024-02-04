5af0e299dc883552a63e463aa2df28f9f38b84e4
Abhishek Kumar <avykr07@gmail.com>
2024-02-05 01:51:49 +0530

    static component is completed

--- src/App.js
+++ src/App.js
@@ -23,26 +23,43 @@ const initialFriends = [
 
 export default function App() {
 	const [friendsARR, SetFriendsARR] = useState(initialFriends);
+	const [showMainLogic, setShowMainLogic] = useState(false);
+	const [userId, SetUserId] = useState();
+
+	function handleAddNewFren(obj) {
+		SetFriendsARR((prev) => [...prev, obj]);
+	}
+	function toggleShowMainLogic(value) {
+		setShowMainLogic((prev) => !prev);
+		SetUserId(value);
+	}
 
 	return (
 		<div className="app">
-			<FriendUI friendsARR={friendsARR} />
+			<FriendUI
+				friendsARR={friendsARR}
+				onAddFren={handleAddNewFren}
+				onSelect={toggleShowMainLogic}
+			/>
+			{showMainLogic && (
+				<MainLogic userId={userId} friendsARR={friendsARR} />
+			)}
 		</div>
 	);
 }
 
-function FriendUI({ friendsARR }) {
+function FriendUI({ friendsARR, onAddFren, onSelect }) {
 	const [addNewFren, SetAddNewFren] = useState(false);
 	return (
 		<div className="sidebar">
 			<ul>
 				{friendsARR.map((ele) => (
 					<li key={ele.id}>
-						<FriendList obj={ele} />
+						<FriendList obj={ele} onSelect={onSelect} />
 					</li>
 				))}
 			</ul>
-			{addNewFren && <AddFriendForm />}
+			{addNewFren && <AddFriendForm onAddFren={onAddFren} />}
 			<button
 				className="button"
 				onClick={() => SetAddNewFren((prev) => !prev)}
@@ -53,24 +70,112 @@ function FriendUI({ friendsARR }) {
 	);
 }
 
-function FriendList({ obj }) {
+function FriendList({ obj, onSelect }) {
+	const [hasSelect, setHasSelect] = useState(false);
+	function handleClick(val) {
+		onSelect(val);
+		setHasSelect((prev) => !prev);
+	}
+
 	return (
 		<>
 			<img src={obj.image} />
 			<h3>{obj.name}</h3>
-			<p>{obj.balance}</p>
-			<button className="button">Select</button>
+			<p>
+				{obj.balance < 0 && (
+					<span className="red">
+						You owe {obj.name + " " + obj.balance} $
+					</span>
+				)}
+			</p>
+			<button className="button" onClick={() => handleClick(obj.id)}>
+				{hasSelect ? "Close" : "Select"}
+			</button>
 		</>
 	);
 }
 
-function AddFriendForm() {
+function AddFriendForm({ onAddFren }) {
+	const [name, setName] = useState("");
+	const [url, setURL] = useState("");
+
+	function handleAddFren(e) {
+		e.preventDefault();
+		if (name === "") return;
+		const obj = {
+			id: Date.now(),
+			name,
+			url,
+			balance: 0,
+		};
+		onAddFren(obj);
+		setName("");
+		setURL("");
+	}
 	return (
-		<form className=".form-add-friend">
-			<label htmlFor="frenName">Friend name</label>
-			<input type="text" id="frenName" value="" />
-			<label htmlFor="imgUrl">Image URL</label>
-			<input type="url" id="imgUrl" value="" />
+		<form className="form-add-friend">
+			<label htmlFor="frenName">🐥Friend name</label>
+			<input
+				type="text"
+				id="frenName"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
+			<label htmlFor="imgUrl">🌄Image URL</label>
+			<input
+				type="url"
+				id="imgUrl"
+				value={url}
+				onChange={(e) => setURL(e.target.value)}
+			/>
+			<button className="button" onClick={(e) => handleAddFren(e)}>
+				Add
+			</button>
+		</form>
+	);
+}
+
+function MainLogic({ userId, friendsARR, SETSOMEVALUE }) {
+	const obj = friendsARR.filter((ele) => ele.id == userId);
+	const friendName = obj[0].name;
+	const [user, setUser] = useState("myself");
+	return (
+		<form className="form-split-bill">
+			<h2>split a bill with Abhishek</h2>
+
+			<label htmlFor="billValue">💰 Bill Value</label>
+			<input
+				type="number"
+				id="billValue"
+				value={SETSOMEVALUE}
+				onChange={SETSOMEVALUE}
+			/>
+			<label htmlFor="myExpense">😎Your Expense</label>
+			<input
+				type="number"
+				id="myExpense"
+				value={SETSOMEVALUE}
+				onChange={SETSOMEVALUE}
+			/>
+
+			<label htmlFor="frenExpense">👻{friendName}'s Expense</label>
+			<input
+				type="number"
+				id="frenExpense"
+				value={SETSOMEVALUE}
+				onChange={SETSOMEVALUE}
+			/>
+
+			<label htmlFor="payee">🤑 Who's paying the bill?</label>
+			<select
+				id="payee"
+				value={user}
+				onChange={(e) => setUser(e.target.value)}
+			>
+				<option value={user}>Myself</option>
+				<option value={user}>{friendName}</option>
+			</select>
+			<button className="button">Split Bill</button>
 		</form>
 	);
 }
